e024ccf8ef3292126b7c8b7d0187610e80106aad
David <davidalexandredantas@gmail.com>
2023-04-09 14:43:36 +0100

    added constructors and getters to Bill

--- src/main/java/pt/ul/fc/css/democracia2/domain/Bill.java
+++ src/main/java/pt/ul/fc/css/democracia2/domain/Bill.java
@@ -1,16 +1,61 @@
 package pt.ul.fc.css.democracia2.domain;
 
 import java.time.LocalDateTime;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Bill {
+
   private String title;
   private BillStatus status;
   private String description;
   private byte[] file;
-  private LocalDateTime validady;
+  private LocalDateTime validaty;
   private Topic topic;
 
   private VoteBox votes;
   private List<Citizen> supporters;
+
+  public Bill(String title, String description, byte[] file, LocalDateTime validaty, Topic topic) {
+    super();
+    this.title = title;
+    this.description = description;
+    this.file = file;
+    this.validaty = validaty;
+    this.topic = topic;
+
+    this.supporters = new LinkedList<Citizen>();
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public BillStatus getStatus() {
+    return status;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public byte[] getFile() {
+    return file;
+  }
+
+  public LocalDateTime getValidaty() {
+    return validaty;
+  }
+
+  public Topic getTopic() {
+    return topic;
+  }
+
+  public VoteBox getVotes() {
+    return votes;
+  }
+
+  public List<Citizen> getSupporters() {
+    return supporters;
+  }
 }
