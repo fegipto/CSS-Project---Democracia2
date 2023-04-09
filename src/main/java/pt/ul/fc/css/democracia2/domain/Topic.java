c4ee3b9187518b843e2c211d05f692ad7dec6c32
David <davidalexandredantas@gmail.com>
2023-04-09 14:06:11 +0100

    Added Topic class

---
+++ src/main/java/pt/ul/fc/css/democracia2/domain/Topic.java
@@ -0,0 +1,47 @@
+package pt.ul.fc.css.democracia2.domain;
+
+import java.util.Objects;
+
+public class Topic {
+	
+	
+	private Topic parent;
+	public Topic getParent() {
+		return parent;
+	}
+
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public Topic(String name, Topic parent) {
+		this.name = name;
+		this.parent = parent;
+	}
+
+	@Override
+	public String toString() {
+		return parent + ">"+name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, parent);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topic other = (Topic) obj;
+		return Objects.equals(name, other.name) && Objects.equals(parent, other.parent);
+	}
+	
+	
+}
