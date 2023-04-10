cf6b563b9e46cbd670f4fd5fbd3b67b76f978f7e
fc56272 <fc56272@alunos.fc.ul.pt>
2023-04-10 13:23:10 +0100

    Delegate functions

--- src/main/java/pt/ul/fc/css/democracia2/domain/Delegate.java
+++ src/main/java/pt/ul/fc/css/democracia2/domain/Delegate.java
@@ -1,3 +1,93 @@
 package pt.ul.fc.css.democracia2.domain;
 
-public class Delegate extends Citizen {}
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Delegate extends Citizen {
+
+    private final Set<Bill> bills;
+
+    /**
+     * Constructs a new Delegate object using a name,cc,token.
+     *
+     * @param name  the name of the delegate
+     * @param cc    the cc of the delegate
+     * @param token authentication token
+     */
+    public Delegate(String name, long cc, long token) {
+        super(name, cc, token);
+        bills = new HashSet<>();
+    }
+
+    /**
+     * Method that get the name of the Delegate
+     *
+     * @return the name of the corresponding Delegate
+     */
+    public String getName() {
+        return super.getName();
+    }
+
+    /**
+     * Method that get the cc of the Delegate
+     *
+     * @return the cc of the corresponding Delegate
+     */
+    public long getCC() {
+        return super.getCC();
+    }
+
+    /**
+     * Method that get the token of the Delegate
+     *
+     * @return the token of the corresponding Delegate
+     */
+    public long getToken() {
+        return super.getToken();
+    }
+
+    /**
+     * Method that given a Delegate and a Topic, creates a new ChosenDelegate and add it to the
+     * delegates of the delegate
+     *
+     * @param delegate the Delegate to add
+     * @param topic the Topic to add
+     * @requires delegate != null && topic != null
+     */
+    public void chooseDelegate(Delegate delegate, Topic topic) { super.chooseDelegate(delegate, topic); }
+
+    /**
+     * Method that given a Topic return the delegate chosen for that topic
+     *
+     * @param topic the Topic to search
+     * @return the delegate related to that topic || null if doent exist
+     * @requires topic != null
+     */
+    public Delegate getChosenDelegate(Topic topic) {
+        return super.getChosenDelegate(topic);
+    }
+
+    /**
+     * Method that given a Topic checks if there are any ChosenDelegates in delegates with that topic
+     *
+     * @param topic the Topic to check
+     * @return true, if exists a chosenDelegate with that topic. false, otherwise.
+     */
+    public boolean delegateExists(Topic topic) {
+        return super.delegateExists(topic);
+    }
+
+    /**
+     * Method that proposes a Bill
+     *
+     * @param title the Bill's title
+     * @param description the Bill's description
+     * @param file the Bill's file
+     * @param validaty the Bill's validaty
+     * @param topic the Bill's topic
+     */
+    public void proposeBill(String title, String description, byte[] file, LocalDateTime validaty, Topic topic) {
+        bills.add(new Bill(title, description, file, validaty, topic));
+    }
+}
