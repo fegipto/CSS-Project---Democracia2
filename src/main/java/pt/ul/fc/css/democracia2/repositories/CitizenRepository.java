0ed7e1b7587e5eac24954c60a2ad3845777edc31
RafaelNisa <rafaelnisa.2001@hotmail.com>
2023-04-11 17:40:37 +0100

    Fixed the addCitizen. Implemented addDelegate.

--- src/main/java/pt/ul/fc/css/democracia2/repositories/CitizenRepository.java
+++ src/main/java/pt/ul/fc/css/democracia2/repositories/CitizenRepository.java
@@ -1,17 +1,31 @@
 package pt.ul.fc.css.democracia2.repositories;
 
+import javax.decorator.Delegate;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import pt.ul.fc.css.democracia2.domain.Citizen;
+import pt.ul.fc.css.democracia2.domain.Topic;
 
 public interface CitizenRepository extends JpaRepository<Citizen, Long> {
   @Query("SELECT c FROM Citizen c WHERE c.cc = :cc")
   Citizen getCitizen(@Param("cc") Long cc);
 
   @Modifying
-  @Query("INSERT INTO Citizen (param1, param2, param3) VALUES (:param1, :param2, :param3)")
+  @Query(
+      "INSERT INTO Citizen (CIT_NAME, CIT_CC, CIT_TOKEN) VALUES (:citizenName, :citizenCC, :citizenToken)")
   void addCitizen(
-      @Param("param1") String name, @Param("param2") long cc, @Param("param3") long token);
+      @Param("citizenName") String name,
+      @Param("citizenCC") long cc,
+      @Param("citizenToken") long token);
+
+  @Modifying
+  @Transactional
+  @Query("UPDATE Citizen c SET c.chosenDelegate[:topic] = :delegate WHERE c.id = :citizenId")
+  void addDelegate(
+      @Param("citizenId") Long citizenId,
+      @Param("topic") Topic topic,
+      @Param("delegate") Delegate delegate);
 }
