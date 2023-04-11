f07c9fc69498999b319af7ee04bf157b39b2342c
RafaelNisa <rafaelnisa.2001@hotmail.com>
2023-04-11 16:56:18 +0100

    getTopics implemented

--- src/main/java/pt/ul/fc/css/democracia2/repositories/TopicRepository.java
+++ src/main/java/pt/ul/fc/css/democracia2/repositories/TopicRepository.java
@@ -1,8 +1,12 @@
 package pt.ul.fc.css.democracia2.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pt.ul.fc.css.democracia2.domain.Topic;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
-    
+
+  @Query("SELECT t.name FROM Topic t")
+  List<Topic> getTopics();
 }
