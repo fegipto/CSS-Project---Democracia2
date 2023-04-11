95cd8474a0c127d8f44664f2fd75a3a1a0a54d01
RafaelNisa <rafaelnisa.2001@hotmail.com>
2023-04-11 17:16:45 +0100

    Implemented  addCitizen()

--- src/main/java/pt/ul/fc/css/democracia2/repositories/CitizenRepository.java
+++ src/main/java/pt/ul/fc/css/democracia2/repositories/CitizenRepository.java
@@ -1,11 +1,17 @@
 package pt.ul.fc.css.democracia2.repositories;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import pt.ul.fc.css.democracia2.domain.Citizen;
 
 public interface CitizenRepository extends JpaRepository<Citizen, Long> {
-  @Query("SELECT t FROM Citizen t WHERE t.cc = :cc")
+  @Query("SELECT c FROM Citizen c WHERE c.cc = :cc")
   Citizen getCitizen(@Param("cc") Long cc);
+
+  @Modifying
+  @Query("INSERT INTO Citizen (param1, param2, param3) VALUES (:param1, :param2, :param3)")
+  void addCitizen(
+      @Param("param1") String name, @Param("param2") long cc, @Param("param3") long token);
 }
