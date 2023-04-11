f62b14e19e012736806ab2e7e5380611989efc31
RafaelNisa <rafaelnisa.2001@hotmail.com>
2023-04-11 17:45:14 +0100

    Implemented getVotableBills

--- src/main/java/pt/ul/fc/css/democracia2/repositories/BillRepository.java
+++ src/main/java/pt/ul/fc/css/democracia2/repositories/BillRepository.java
@@ -1,8 +1,12 @@
 package pt.ul.fc.css.democracia2.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pt.ul.fc.css.democracia2.domain.Bill;
 
 public interface BillRepository extends JpaRepository<Bill, Long> {
-    
+
+  @Query("SELECT b FROM Bill WHERE b.status = 'VOTING'")
+  List<Bill> getVotableBills();
 }
