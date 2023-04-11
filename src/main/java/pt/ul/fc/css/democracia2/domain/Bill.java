96ec0de80f43b6d6bb021e7fd90a927a94ea33a3
David <davidalexandredantas@gmail.com>
2023-04-11 10:49:06 +0100

    Added Bill as an Entity

--- src/main/java/pt/ul/fc/css/democracia2/domain/Bill.java
+++ src/main/java/pt/ul/fc/css/democracia2/domain/Bill.java
@@ -1,28 +1,79 @@
 package pt.ul.fc.css.democracia2.domain;
 
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.GenerationType.AUTO;
+
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+@Entity
 public class Bill {
 
+  public Bill() {
+    // No-argument constructor
+  }
+
+  @Id
+  @GeneratedValue(strategy = AUTO)
+  @Column(name = "BILL_ID")
+  private long id;
+
+  @Column(name = "BILL_TITLE")
   private String title;
+
+  @Enumerated(STRING)
+  @Column(name = "BILL_STAT")
   private BillStatus status;
+
+  @Column(name = "BILL_DESC")
   private String description;
-  private byte[] file;
-  private LocalDateTime validaty;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "BILL_VALIDITY")
+  private Date validity;
+
+  @ManyToOne
+  @JoinColumn(name = "TOPIC_ID")
   private Topic topic;
 
-  private VoteBox voteBox;
-  private List<Citizen> supporters;
+  @Embedded private VoteBox voteBox;
 
-  public Bill(String title, String description, byte[] file, LocalDateTime validaty, Topic topic) {
+  @ManyToOne private Delegate proponent;
+
+  @OneToMany private List<Citizen> supporters;
+
+  @Column(name = "BILL_FILE")
+  @Lob
+  private byte[] file;
+
+  public Bill(
+      String title,
+      String description,
+      byte[] file,
+      Date validity,
+      Topic topic,
+      Delegate proponent) {
     super();
     this.title = title;
     this.description = description;
     this.file = file;
-    this.validaty = validaty;
+    this.validity = validity;
     this.topic = topic;
+    this.proponent = proponent;
 
     this.supporters = new LinkedList<Citizen>();
   }
