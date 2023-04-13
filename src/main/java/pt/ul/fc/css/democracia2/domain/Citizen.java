47324cd210ae8b466196107277914aa788b0fd06
David <davidalexandredantas@gmail.com>
2023-04-13 16:50:27 +0100

    changed citizens id to be his cc

--- src/main/java/pt/ul/fc/css/democracia2/domain/Citizen.java
+++ src/main/java/pt/ul/fc/css/democracia2/domain/Citizen.java
@@ -1,6 +1,5 @@
 package pt.ul.fc.css.democracia2.domain;
 
-import static javax.persistence.GenerationType.AUTO;
 import static javax.persistence.InheritanceType.SINGLE_TABLE;
 
 import java.util.HashMap;
@@ -8,7 +7,6 @@ import java.util.Map;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.JoinColumn;
@@ -25,14 +23,10 @@ public class Citizen {
     // No-argument constructor
   }
 
-  @Id
-  @GeneratedValue(strategy = AUTO)
-  @Column(name = "CIT_ID")
-  private Long id;
-
   @Column(name = "CIT_NAME")
   private String name;
 
+  @Id
   @Column(name = "CIT_CC")
   private long cc;
 
