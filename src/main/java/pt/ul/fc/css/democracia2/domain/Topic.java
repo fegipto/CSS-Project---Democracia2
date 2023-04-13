a7b4df09059de0978546355fe4b88a3371009cdc
David <davidalexandredantas@gmail.com>
2023-04-13 15:54:30 +0100

    added Topic as a entity

--- src/main/java/pt/ul/fc/css/democracia2/domain/Topic.java
+++ src/main/java/pt/ul/fc/css/democracia2/domain/Topic.java
@@ -1,49 +1,55 @@
 package pt.ul.fc.css.democracia2.domain;
 
 import java.util.Objects;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import org.springframework.lang.NonNull;
 
+@Entity
 public class Topic {
 
+  @Id @GeneratedValue private Long id;
 
-	private Topic parent;
-	public Topic getParent() {
-		return parent;
-	}
+  @ManyToOne
+  @JoinColumn(name = "parent_id")
+  private Topic parent;
 
-	private String name;
+  public Topic() {}
 
-	public String getName() {
-		return name;
-	}
+  public Topic getParent() {
+    return parent;
+  }
 
-	public Topic(@NonNull String name, Topic parent) {
-		this.name = name;
-		this.parent = parent;
-	}
+  private String name;
 
-	@Override
-	public String toString() {
-		return parent + ">"+name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(name, parent);
-	}
+  public Topic(@NonNull String name, Topic parent) {
+    this.name = name;
+    this.parent = parent;
+  }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Topic other = (Topic) obj;
-		return Objects.equals(name, other.name) && Objects.equals(parent, other.parent);
-	}
+  @Override
+  public String toString() {
+    return parent + ">" + name;
+  }
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, parent);
+  }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Topic other = (Topic) obj;
+    return Objects.equals(name, other.name) && Objects.equals(parent, other.parent);
+  }
 }
