9a23c8722cfe34b49aef4c9753202e89e09c3534
David <davidalexandredantas@gmail.com>
2023-04-17 20:59:17 +0100

    added individual bill get

--- src/main/java/pt/ul/fc/css/democracia2/services/ConsultNonExpiredBillService.java
+++ src/main/java/pt/ul/fc/css/democracia2/services/ConsultNonExpiredBillService.java
@@ -2,10 +2,12 @@ package pt.ul.fc.css.democracia2.services;
 
 import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pt.ul.fc.css.democracia2.DTO.BillDTO;
+import pt.ul.fc.css.democracia2.domain.Bill;
 import pt.ul.fc.css.democracia2.domain.BillStatus;
 import pt.ul.fc.css.democracia2.repositories.BillRepository;
 
@@ -23,5 +25,10 @@ public class ConsultNonExpiredBillService {
     return billRepository.getBillsNotInStatus(BillStatus.EXPIRED).stream()
         .map(bill -> new BillDTO(bill))
         .collect(Collectors.toList());
+  }
+
+  public Optional<BillDTO> getBill(Long id) {
+    Optional<Bill> bill = billRepository.findById(id);
+    return (bill.isPresent()) ? Optional.of(new BillDTO(bill.get())) : Optional.empty();
   }
 }
